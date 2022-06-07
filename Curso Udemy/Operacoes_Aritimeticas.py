b004ab4e07b0d48840e7de1c5daadcb3bacd9d78
Rafael Caronezi <caronezi@yahoo.com.br>
2022-06-06 21:18:30 -0300

    Comit dia 06/06/22
    Ola Mundo  e  operadores matematicos

---
+++ Curso Udemy/Operacoes_Aritimeticas.py
@@ -0,0 +1,16 @@
+#Operações aritimeticas 
+a = 10 
+b = 3
+# Soma 
+
+print(a+b)
+
+#subtração 
+print(a-b)
+#subtração com resultado negativo
+print(b-a)
+#mutiplicação
+print(a*b)
+
+#divisao
+print(a/b)
